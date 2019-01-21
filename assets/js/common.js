2beaea088e8d4b260a612262207593f4fb129780
DanzigWebb <42899726+DanzigWebb@users.noreply.github.com>
2019-01-22 00:37:25 +0300

    Залив

---
+++ assets/js/common.js
@@ -0,0 +1,35 @@
+
+
+$(document).ready(function() {
+    $("#my-menu").mmenu({
+        extensions: [ 'effect-menu-slide', 'pagedim-black', "fx-listitems-fade", 'position-right', 'theme-dark' ],
+        navbar: {
+            title: '<img src="https://livedemo00.template-help.com/wt_prod-16368/images/logo-181x25.png">'
+        },
+        "counters": true,
+                    "iconbar": {
+                    "add": true,
+                    "top": [
+                        "<a href='/'><i class='fa fa-home'></i></a>",
+                    ],
+                    "bottom": [
+                        "<a href='https://vk.com/id128989093' target='_blank'><i class='fab fa-vk'></i></a>",
+                        "<a href='#/'><i class='fas fa-envelope'></i></a>",
+                        "<a href='https://api.whatsapp.com/send?phone=79500247418' target='_blank'><i class='fab fa-whatsapp'></i></a>"
+                    ],
+                    }
+    }, {
+        // configuration
+        offCanvas: {
+            pageSelector: "#my-page"
+        }
+    });
+    
+    var api = $('#my-menu').data('mmenu');
+    api.bind('open:start', function() {
+        $('.hamburger').addClass('is-active');
+    });
+    api.bind('close:before', function() {
+        $('.hamburger').removeClass('is-active');
+        })
+    });
